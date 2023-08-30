832d487340d6ef35fa42a4777b704d2b4c837147
Simple <huyang1994822@gmail.com>
2023-08-31 00:00:02 +0800

    add StarRailWish.vue WishList.vue

---
+++ src/components/StarRailWish.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "StarRailWish"
+}
+</script>
+
+<template>
+<div id="srWish">
+  <div id="characterWish"></div>
+  <div id="weaponWish"></div>
+  <div id="standardWish"></div>
+  <div id="beginnerWish"></div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/WishList.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: "Wish"
+}
+</script>
+
+<template>
+<div id="wishTime"></div>
+<div id="wishName"></div>
+<div id="wishStar"></div>
+<div id="wishType"></div>
+<div id="wishRoll"></div>
+</template>
+
+<style scoped>
+
+</style>
