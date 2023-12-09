91b1c2d34e8d32a4eca7da4fe477bf863f008556
Simple <huyang1994822@gmail.com>
2023-12-09 13:22:27 +0800

    p

--- src/components/bottom/index.vue
+++ src/components/bottom/index.vue
@@ -6,7 +6,7 @@
   <div class="bottom">
     <div class="content">
       <div class="left">
-        <a href="https://www.bilibili.com/video/BV1CN41167AC?p=12" target="_blank">参考视频</a>
+        <a href="https://www.bilibili.com/video/BV1CN41167AC?p=14" target="_blank">参考视频</a>
       </div>
       <div class="right">
         <span>我的Blog</span>
